1376b9ec19ac79a1af8c3faea1d2ceaf5ccc5b90
Ariel <ariel-evaldt@hotmail.com>
2019-05-30 21:56:23 -0300

    version 1.0.3

--- _javaScript/Trash.js
+++ _javaScript/Trash.js
@@ -1,15 +1,23 @@
 class Trash {
-    constructor(x, y, size, img){
+    constructor(x, y, size, img, type){
         this.x = x;
         this.y = y;
         this.img = img;
         this.size = size;
-        this.type;
+        this.type = type;
         this.held = false;
     }
     
     setHeld(held){
         this.held = held;
+    }
+    
+    setX(x){
+        this.x = x;
+    }
+    
+    setY(y){
+        this.y = y;
     }
     
     getSize(){
@@ -20,6 +28,10 @@ class Trash {
         return this.held;
     }
     
+    getType(){
+        return this.type;
+    }
+    
     show(){
 //        fill(255);
 //        noStroke();
@@ -28,17 +40,21 @@ class Trash {
     }
     
     click(){
-        if((mouseX > this.x && mouseX < this.x+ this.size) && (mouseY > this.y && mouseY < this.y+this.size)){
+        if((mouseX+26 > this.x && mouseX+26 < this.x+ this.size) && (mouseY+27 > this.y && mouseY+27 < this.y+this.size)){
             return true;
         }
     }
     
     move(){
-        if(mouseX > this.size/2 && mouseX < width-this.size/2){
-            this.x = mouseX - this.size/2;
+        if((mouseX > this.size/2 && mouseX < width-this.size/2)){
+            if(mouseX < width-width*0.25-this.size/2 || mouseY > height*0.2){
+                this.x = mouseX+26 - this.size/2;
+            }
         }
         if(mouseY <= height-this.size/2 && mouseY > this.size/2) {
-            this.y = mouseY - this.size/2;
+            if(mouseX < width-width*0.25 || mouseY > height*0.2+this.size/2){
+                this.y = mouseY+26 - this.size/2;
+            }
         }
     }
 }
--- _javaScript/reciclagem.js
+++ _javaScript/reciclagem.js
@@ -18,6 +18,8 @@ let trashImages = [];
 //fim imagens
 
 //variáveis de controle
+
+//lixos
 let qtdMetal = 5;
 let qtdPaper = 2;
 let qtdPlastic = 2;
@@ -25,16 +27,32 @@ let qtdOrganic = 1;
 let qtdGlass = 2;
 let qtdTotal = qtdMetal + qtdPaper + qtdPlastic + qtdOrganic + qtdGlass;
 
+let initialX;
+let initialY;
+
 let heldTrash;
+
+//timer
+let minute = 0;
+let second = 0;
+let currentTime;
+let timerOn = false;
+
+//elementos dos modais
+let scoreField = document.getElementById("scoreField");
+let btnTryAgain = document.getElementById("btnTryAgain");
+let timeSelect = document.getElementById("timeSelect");
+let btnStart = document.getElementById("btnStart");
+
 //fim variáveis de controle
-
-
 
 //fontes
 let boogaloo;
 //fim fontes
+let ground;
 
-let score = 240;
+
+let score = 0;
 
 //variáveis do banco
 let username = 'arielsch';
@@ -88,6 +106,7 @@ function preload(){
     
     //carrega a fonte boogaloo
     boogaloo = loadFont('_fonts/Boogaloo-Regular.otf');
+    //ground = loadImage('_images/ground.jpg')
 }
 
 let counterSize = 0;
@@ -97,16 +116,48 @@ let trashSize = [0.08,0.12,0.08,0.1,0.12];
 function setup() {
   createCanvas(width,height);
     for(let i = 0; i < qtdTotal; i++){
-        trashes[i]= new Trash(random(0, width-height*trashSize[sizeArray[i]]), random(height*0.5, height-height*trashSize[sizeArray[i]]), height*trashSize[sizeArray[i]], trashImages[i]);
+        trashes[i]= new Trash(random(0, width-height*trashSize[sizeArray[i]]), random(height*0.5, height-height*trashSize[sizeArray[i]]), height*trashSize[sizeArray[i]], trashImages[i], sizeArray[i]);
     }
     for(let i = 0; i < 5; i++){
-        cans[i] = new Can(i*width*0.1 + 100, height*0.55-height*0.3, height*0.3, width*0.1, i+1, canImages[i]);
+        cans[i] = new Can(i*width*0.1 + 100, height*0.55-height*0.3, height*0.3, width*0.1, i, canImages[i]);
         //can(x,y,height,width,type,img);
     }
+    
+    noLoop();
+    $('#inicioModal').modal('show');
 }
 
 function draw() {
+//Time setting
+    if(timerOn){
+        if((millis()-currentTime) >= 1000){
+            if(second > 0){
+                second--;
+            } else{
+                minute--;
+                second = 59;
+            }
+            currentTime = millis();
+            if(minute == 0 && second == 0){
+                $("#resultadoModal").modal("show");
+                scoreField.value = score;
+                timerOn = false;
+                noLoop();
+            }
+        }
+    }
+//fim time setting
+    
   background('#81d8d0');
+//Timer
+    noStroke();
+    fill(255);
+    textFont(boogaloo);
+    textSize(width*0.05);
+    text(nf(minute,2) + ":" + nf(second,2), width*0.01,width*0.04);
+    stroke(0);
+//fim Timer
+    
 //scoreboard
   fill(255);
   rect(width-width*0.25, 0, width*0.25, height*0.2);
@@ -128,6 +179,7 @@ function draw() {
   fill('#8dce55');
   stroke('#0B5345');
   rect(0, height*0.5, width,height*0.5);
+    //image(ground, 0, height*0.5, width, height*0.5);
 //fim chão
     //desenha as latas de lixo
     for(let can of cans){
@@ -139,13 +191,24 @@ function draw() {
         trash.show();
     }
     //fim lixos
+    
+    //abre a aba de resultados se não tiver mais lixos
+    if(trashes.length == 0){
+        $("#resultadoModal").modal("show");
+        scoreField.value = score;
+        timerOn = false;
+    }
 }
 
 function mousePressed(){
+    
     for(let i = 0; i< qtdTotal; i++){
        if(trashes[i].click()){
            trashes[i].setHeld(true);
            heldTrash = i;
+           initialX = trashes[heldTrash].x;
+           initialY = trashes[heldTrash].y;
+           document.getElementsByTagName('canvas')[0].style.cursor = "url('_images/hand-cursor-closed.cur'), auto";
            break;
        } 
     }
@@ -155,16 +218,35 @@ function mousePressed(){
 //////possível otimização/////// ==== armazenar o indíce do objeto que está sendo arrastado para assim desativar o movimento somente deste único objeto e n de todo o array
 function mouseReleased(){
     for(let i = 0; i < 5; i++){
-            if((trashes[heldTrash].x > cans[i].x && trashes[heldTrash].x < cans[i].x + cans[i].width)&&(trashes[heldTrash].y > cans[i].y && trashes[heldTrash].y < cans[i].y + cans[i].height/2)){
+            if((trashes[heldTrash].x > cans[i].x && trashes[heldTrash].x < cans[i].x + cans[i].width)&&(trashes[heldTrash].y > cans[i].y && trashes[heldTrash].y < cans[i].y + cans[i].height)){
                 console.log('inside');
+                if(trashes[heldTrash].getType() == cans[i].getType()){
+                    score += 10;
+                    console.log(score);
+                } else {
+                    score -= 5;
+                }
                 trashes.splice(heldTrash,1);
-            } else {
-                trashes[heldTrash].setHeld(false); 
+                break;
+                console.log('entrou if check inside');
             } 
+            if(i == 4) {
+                if(trashes[heldTrash].y < height*0.5){
+                    trashes[heldTrash].setX(initialX);
+                    trashes[heldTrash].setY(initialY);
+                    trashes[heldTrash].setHeld(false);  
+                    console.log('entrou else 1');
+                } 
+                else {
+                    trashes[heldTrash].setHeld(false);
+                    console.log('entrou else 2');
+                }
+            }
     }
 //    for(let trash of trashes){
 //        trash.setHeld(false); 
 //    }
+    document.getElementsByTagName('canvas')[0].style.cursor = "url('_images/hand-cursor-open.cur'), auto";
 }
 ////fim possível otimização/////
 
@@ -175,4 +257,45 @@ function mouseDragged(){
             trash.move();
         }
     }
+//    trashes[heldTrash].move();   
 }
+
+function tryAgain(){
+    window.location.reload();
+}
+
+function startGame(){
+    timerOn = true;
+    switch (timeSelect.value){
+        case '0':
+            minute = 0;
+            second = 0;
+            timerOn = false;
+            break;
+        case '1':
+            minute = 0;
+            second = 10;
+            break;
+        case '2':
+            minute = 0;
+            second = 30;
+            break;
+        case '3':
+            minute = 1;
+            second = 0;
+            break;
+        case '4':
+            minute = 1;
+            second = 30;
+            break;
+        case '5':
+            minute = 2;
+            second = 0;
+    }
+    currentTime = millis();
+    loop();
+}
+
+btnTryAgain.addEventListener('click', tryAgain);
+btnStart.addEventListener('click', startGame);
+
